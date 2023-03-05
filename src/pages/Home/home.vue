f50849f0ed675c33642fe539b9c6f9dcf4882cc3
D-Sketon <2055272094@qq.com>
2023-03-05 18:35:47 +0800

    chore: 引入eslint和prettier

--- src/pages/Home/home.vue
+++ src/pages/Home/home.vue
@@ -1,12 +1,7 @@
-<template>
-    
-</template>
+<template></template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
+import { ref } from 'vue';
 </script>
 
-<style lang="scss">
-    
-</style>
+<style lang="scss"></style>

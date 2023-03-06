1765ed117cb9551b1098a8a0706f7846a608da5d
D-Sketon <2055272094@qq.com>
2023-03-06 11:51:11 +0800

    style: prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     printWidth: 100,
     tabWidth: 4,
-    useTabs: true,
+    useTabs: false,
     semi: true,
     singleQuote: true,
     trailingComma: 'none',
--- src/pages/LawQuery/law_query.vue
+++ src/pages/LawQuery/law_query.vue
@@ -1,12 +1,12 @@
 <template>
-	<view class="container">
-		<image class="mainPage" src="@/static/image/main_page.png" mode="widthFix" />
-		<view class="mask"></view>
-		<view class="textContainer">
-			<view class="text lawText">法律</view>
-			<view class="text">查询</view>
-		</view>
-	</view>
+    <view class="container">
+        <image class="mainPage" src="@/static/image/main_page.png" mode="widthFix" />
+        <view class="mask"></view>
+        <view class="textContainer">
+            <view class="text lawText">法律</view>
+            <view class="text">查询</view>
+        </view>
+    </view>
 </template>
 
 <script setup lang="ts">
@@ -15,37 +15,37 @@ import { ref } from 'vue';
 
 <style lang="scss" scoped>
 .container {
-	position: relative;
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	justify-content: center;
+    position: relative;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
 }
 image {
-	width: 100%;
+    width: 100%;
 }
 .mask {
-	position: absolute;
-	top: 0;
-	width: 100%;
-	height: 100%;
-	background-color: rgba($color: #000000, $alpha: 0.4);
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba($color: #000000, $alpha: 0.4);
 }
 .textContainer {
-	margin-left: 50upx;
-	display: flex;
-	flex-direction: row;
-	position: absolute;
+    margin-left: 50upx;
+    display: flex;
+    flex-direction: row;
+    position: absolute;
 }
 .text {
-	font-size: 50upx;
-	// font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-	font-style: 900;
-	color: white;
-	letter-spacing: 2upx;
+    font-size: 50upx;
+    // font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    font-style: 900;
+    color: white;
+    letter-spacing: 2upx;
 }
 .lawText {
-	color: $main-color;
+    color: $main-color;
 }
 </style>
